f13dc219216da4f66d1fc8f67edb783bd333b552
Kliment_Petkov <klimopetkov@gmail.com>
2022-04-11 21:41:35 +0300

    Added configuration

---
+++ app/Config/Config.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Config;
+
+class Config {
+    const CUSTOMER_RESOURCE_PATH = __DIR__ . '\..\..\RawData\customer-channel.txt';
+    const USER_RESOURCE_PATH = __DIR__ . '\..\..\RawData\user-channel.txt';
+}
